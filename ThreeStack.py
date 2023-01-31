5ef3748ce2a688cd415fd201aee3e17f6d9223af
Nilia BARDACHENE <99604098+Nilia001@users.noreply.github.com>
2023-02-01 00:49:40 +0100

    Update ThreeStack.py

--- ThreeStack.py
+++ ThreeStack.py
@@ -2,14 +2,14 @@ class ThreeStack:
     def init(self):
         self.stacks = [[] for _ in range(3)]
 
-    def push(self, stack_num, obj):
-        if stack_num < 1 or stack_num > 3:
+    def push(self, num, obj):
+        if num < 1 or num > 3:
             raise Exception("Invalid stack number")
-        self.stacks[stack_num-1].append(obj)
+        self.stacks[stack-1].append(obj)
 
-    def pop(self, stack_num):
-        if stack_num < 1 or stack_num > 3:
+    def pop(self, stack):
+        if stack < 1 or stack > 3:
             raise Exception("Invalid stack number")
-        if not self.stacks[stack_num-1]:
+        if not self.stacks[stack-1]:
             raise Exception("Stack is empty")
-        return self.stacks[stack_num-1].pop()
+        return self.stacks[stack-1].pop()
